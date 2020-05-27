ddbb26e4484df710b5709a085587341b1e654d10
Pradeep kumar <pradeepkr1303@gmail.com>
2020-05-27 11:39:50 +0530

    Work done till 26-May-2020

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import * as config from 'config';
 
 async function bootstrap() {
+  const logger = new Logger();
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const serverConfig = config.get('server')
+
+  const port = process.env.PORT || serverConfig.port
+  await app.listen(port);
+  logger.log(`App listening on port "${port}"`)
 }
 bootstrap();
